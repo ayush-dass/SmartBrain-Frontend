590f60229363a009dc0ccc6f673c1b99304a43bc
Ayush Das <ayushdas221202@gmail.com>
2023-08-19 00:16:25 +0530

    changing urls

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ class App extends Component {
         .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch('http://localhost:3000/image', {
+            fetch('https://smartbrain-backend-ehav.onrender.com/image', {
               method: 'put',
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
